916d95717d0d13e7e70a5dc81c4d9e7cbf063ec1
murraycadzow <murray.cadzow@otago.ac.nz>
2019-02-02 18:05:46 +1300

    click input on plot but not providing info yet

--- app.R
+++ app.R
@@ -11,8 +11,11 @@
 
 library(shiny)
 library(tidyverse)
+library(scales)
 library(mortgageCalc)
+library(plotly)
 
+theme_set(theme_minimal())
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -39,51 +42,89 @@ ui <- fluidPage(
                    value = 5
       ),
       textOutput("repayment"),
-      textOutput("totals")
-
+      textOutput("total_interest"),
+      textOutput("total")
     ),
 
 
     # Show a plot of the generated distribution
     mainPanel(
       tabsetPanel(
-        tabPanel(title = "Cumulative",
-                 plotOutput("cumulativePlot")
+        tabPanel(title = "Plots",
+                 tabsetPanel(
+                   tabPanel(title = "Cumlative",
+                            plotOutput("cumulativePlot",
+                                       click = "cumulative_click",
+                                       hover = hoverOpts(id = "cumulative_hover",
+                                                                           delay = 500,
+                                                                           clip = TRUE,
+                                                                           delayType = c('debounce'))),
+                            verbatimTextOutput("hover_info"),
+                            verbatimTextOutput("click_info")
+                   ),
+                   tabPanel(title = "Payment portions",
+                            plotOutput("principleInterestPortionPlot")
 
+                   )
+                 )
         ),
-        tabPanel(title = "table", dataTableOutput("amort_tab"))
+        tabPanel(title = "Table", dataTableOutput("amort_tab"))
       )
     )
   )
 )
 
+
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-  output$repayment <-renderText(
-    paste("Monthly repayment amount:",monthly_payment(principle = input$initialprinciple,
-                    annual_interest = input$interest/100, years = input$years) %>% round(.,2))
-  )
 
   amort <- reactive({amort_table(principle = input$initialprinciple,
-                       annual_interest = input$interest/100,
-                       years = input$years) %>% mutate_at(-1, funs(round(., 2)))
+                                 annual_interest = input$interest/100,
+                                 years = input$years) %>% mutate_at(-1, funs(round(., 2)))
+  })
+
+  output$repayment <-renderText(
+    paste("Monthly repayment amount:",monthly_payment(principle = input$initialprinciple,
+                                                      annual_interest = input$interest/100, years = input$years) %>% dollar())
+  )
+
+  output$principleInterestPortionPlot <- renderPlot({
+    amort() %>% select(payment, contains("portion")) %>%
+      gather( key = "portion", value = "Dollars", contains("portion")) %>%
+      ggplot(aes(payment, Dollars, colour = portion,fill = portion,  group = payment)) +
+      geom_bar(stat = 'identity', position = 'stack') +
+      scale_y_continuous(labels= dollar_format())
   })
 
   output$cumulativePlot <- renderPlot({
-
-
-    amort()%>% select(payment, contains("cumulative")) %>%
+    p<- amort()%>% select(payment, contains("cumulative"), balance) %>%
       gather(key = "category", value = "value", -1 ) %>%
-      ggplot(aes(x = payment, y = value, by = category, colour = category)) + geom_line()
+      ggplot(aes(x = payment, y = value, by = category, colour = category)) +
+      geom_line() +
+      scale_y_continuous(labels= dollar_format())
+    if(!is.null(input$cumulative_hover)){
+      p <- p + geom_vline(xintercept =input$cumulative_hover$x)
+    }
+    return(p)
   })
 
   output$amort_tab <- renderDataTable({
     amort()
   })
 
-  output$totals <- renderText({
-    paste(amort() %>%
-      summarise_at(vars(contains("portion"), month_payment), sum), collapse = " ")
+  output$total_interest <- renderText({
+    paste("Total interest:", amort() %>%
+            summarise(dollar(sum(interest_portion))) )
+  })
+
+  output$total <- renderText({
+    paste("Total:", amort() %>%
+            summarise(dollar(sum(month_payment))) )
+  })
+
+  output$click_info <- renderPrint({
+    cat("input$cumulative_click:\n")
+    str(input$cumulative_click)
   })
 
 }
